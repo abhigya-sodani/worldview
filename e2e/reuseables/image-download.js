ef54f0a140e474563964a7521b9aa3d264d2fba0
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2020-09-10 10:21:09 -0700

    Update image-download.js

--- e2e/reuseables/image-download.js
+++ e2e/reuseables/image-download.js
@@ -2,6 +2,7 @@ const TIME_LIMIT = 10000;
 
 module.exports = {
   openImageDownloadPanel(c) {
+    alert("hello3");
     c.waitForElementNotPresent('.modal fade', TIME_LIMIT);
     c.waitForElementVisible('#wv-image-button', TIME_LIMIT);
     c.click('#wv-image-button');
@@ -9,6 +10,7 @@ module.exports = {
   },
 
   closeImageDownloadPanel(c) {
+    alert("hello2");
     c.waitForElementVisible('#toolbar_snapshot .close', TIME_LIMIT);
     c.click('#toolbar_snapshot .close');
     c.waitForElementNotPresent('#toolbar_snapshot', TIME_LIMIT);
@@ -16,6 +18,7 @@ module.exports = {
   },
 
   clickDownload(c) {
+    alert("hello");
     c.waitForElementNotPresent('.modal fade', TIME_LIMIT);
     c.waitForElementVisible('.wv-image-button', TIME_LIMIT);
     c.click('.wv-image-button');
